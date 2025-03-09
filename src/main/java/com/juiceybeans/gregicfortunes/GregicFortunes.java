e19812b2ce096b592bb723eaabdf14c6dd76778d
Karthi Suresh <75553966+JuiceyBeans@users.noreply.github.com>
2025-03-09 01:08:39 +0000

    Setup

---
+++ src/main/java/com/juiceybeans/gregicfortunes/GregicFortunes.java
@@ -0,0 +1,40 @@
+package com.juiceybeans.gregicfortunes;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import static com.juiceybeans.gregicfortunes.GregicFortunes.MOD_ID;
+
+@Mod(MOD_ID)
+public class GregicFortunes {
+
+    public static final String MOD_ID = "gregicfortunes";
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public GregicFortunes() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+        modEventBus.addListener(this::clientSetup);
+
+        // Most other events are fired on Forge's bus.
+        // If we want to use annotations to register event listeners,
+        // we need to register our object like this!
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        event.enqueueWork(() -> {
+            LOGGER.info("Gregic Fortunes loaded!");
+        });
+    }
+
+    private void clientSetup(final FMLClientSetupEvent event) {
+    }
+}
